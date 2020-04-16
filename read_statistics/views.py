5d5a2a111644e88431564820d9bd53ba30d4a203
AustinWilliam117 <menxu1996@gmail.com>
2020-04-16 13:17:48 +0800

    4.12

--- read_statistics/views.py
+++ read_statistics/views.py
@@ -1,7 +1,30 @@
+import datetime
 from django.shortcuts import render_to_response
 from django.contrib.contenttypes.models import ContentType
-from read_statistics.utils import get_week_read_data
+from django.utils import timezone
+from django.db.models import Sum
+from read_statistics.utils import get_week_read_data, get_today_hot_data, get_yesterday_hot_data
 from blog.models import Blog
+
+def get_week_hot_blogs():
+    today = timezone.now().date()
+    date = today - datetime.timedelta(days=7)
+    blogs = Blog.objects \
+                .filter(read_details__date__lt=today, read_details__date__gte=date) \
+                .values('id', 'title') \
+                .annotate(read_num_sum=Sum('read_details__read_num')) \
+                .order_by('-read_num_sum')
+    return blogs[:7]        
+
+def get_month_hot_blogs():
+    today = timezone.now().date()
+    date = today - datetime.timedelta(days=30)
+    blogs = Blog.objects \
+                .filter(read_details__date__lt=today, read_details__date__gte=date) \
+                .values('id', 'title') \
+                .annotate(read_num_sum=Sum('read_details__read_num')) \
+                .order_by('-read_num_sum')
+    return blogs[:10]   
 
 def read_statistics(request):
     blog_content_type = ContentType.objects.get_for_model(Blog)
@@ -10,4 +33,8 @@ def read_statistics(request):
     context = {}
     context['dates'] = dates
     context['read_nums'] = read_nums
+    context['today_hot_data'] = get_today_hot_data(blog_content_type)
+    context['yesterday_hot_data'] = get_yesterday_hot_data(blog_content_type)
+    context['week_hot_blogs'] = get_week_hot_blogs()
+    context['month_hot_blogs'] = get_month_hot_blogs()
     return render_to_response('read_statistics/read_statistics.html',context)
