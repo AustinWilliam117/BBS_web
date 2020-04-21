61b5487b099da29b84305300ff680b0617183e34
AustinWilliam117 <mengxu1996@gmail.com>
2020-04-21 15:31:45 +0800

    4.20

--- bbs_web/settings.py
+++ bbs_web/settings.py
@@ -142,6 +142,24 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'static')
 # 配置ckeditor
 CKEDITOR_UPLOAD_PATH = 'upload/'
 
+CKEDITOR_CONFIGS = {
+    'comment_ckeditor': {
+        'toolbar': 'custom',
+        'toolbar_custom': [
+            ['Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript'],
+            ["TextColor", "BGColor", 'RemoveFormat'],
+            ['NumberedList', 'BulletedList'],
+            ['Link', 'Unlink'],
+            ["Smiley", "SpecialChar", 'Blockquote'],
+        ],
+        'width': 'auto',
+        'height': '180',
+        'tabSpaces': 4,
+        'removePlugins': 'elementspath',
+        'resize_enabled': False,
+    }
+}
+
 # 自定义参数
 
 # 分页器
